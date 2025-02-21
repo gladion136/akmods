6d604baae2d1ae2d4508320a0528029068cb3454
Etienne Schmidt <gladion136@googlemail.com>
2025-02-21 16:31:02 +0100

    WIP

--- build_files/extra/build-kmod-tuxedo-drivers.sh
+++ build_files/extra/build-kmod-tuxedo-drivers.sh
@@ -19,4 +19,11 @@ for file in /usr/lib/modules/${KERNEL}/extra/tuxedo-drivers/*.ko.xz; do
     || (find /var/cache/akmods/tuxedo-drivers/ -name \*.log -print -exec cat {} \; && exit 1)
 done
 
+# Check if the RPM package is built and available
+RPM_PACKAGE="/var/cache/akmods/tuxedo-drivers/kmod-tuxedo-drivers-${KERNEL}-4.12.1-1.fc${RELEASE}.${ARCH}.rpm"
+if [ ! -f "$RPM_PACKAGE" ]; then
+    echo "ERROR: $RPM_PACKAGE package is not built or not available"
+    exit 1
+fi
+
 rm -f /etc/yum.repos.d/_copr_gladion136-tuxedo-drivers-kmod.repo
